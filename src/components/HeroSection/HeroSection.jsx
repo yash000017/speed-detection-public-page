72db2fd6bedf7e8452e3f493b8cdf572c3d62b55
yash000017 <yashmodiofficial@gmail.com>
2024-12-30 17:53:39 +0530

    final-changes

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './HeroSection.css';
+import React from "react";
+import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
-    <section id='home' className="hero">
+    <section id="home" className="hero">
       <div className="hero-container">
         <div className="hero-title">Revolutionizing Speed Ball Detection</div>
         <div className="hero-description">
-          Our cutting-edge technology uses AI and real-time detection to monitor and analyze speed ball activities.
+          <div className="bg-img"></div>
+          Our cutting-edge technology uses AI and real-time detection to monitor
+          and analyze speed ball activities.
         </div>
       </div>
     </section>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import './Navbar.css';
-import logo from "../../assets/logo.png"
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   // State to track if the navbar should have a solid background
@@ -8,7 +8,8 @@ const Navbar = () => {
 
   const handleScroll = () => {
     // Trigger navbar background change when the page is scrolled by a small amount
-    if (window.scrollY > 50) { // Change "50" to your desired scroll threshold
+    if (window.scrollY > 50) {
+      // Change "50" to your desired scroll threshold
       setScrolled(true);
     } else {
       setScrolled(false);
@@ -17,28 +18,28 @@ const Navbar = () => {
 
   // Use useEffect to add and clean up the scroll event listener
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     // Cleanup function to remove the event listener when the component is unmounted
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <nav className={`navbar ${scrolled ? 'navbar-scrolled' : ''}`}>
+    <nav className={`navbar ${scrolled ? "navbar-scrolled" : ""}`}>
       <div className="navbar-container">
-        <div className='brand'>
-        <img src={logo} className='logo'/>
-        <h1>Ball Speed Detection</h1>
+        <div className="brand">
+          <img src={logo} className="logo" />
+          <h1>Ball Speed Detection</h1>
         </div>
-        <ul>
+        {/* <ul>
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#features">Features</a></li>
           <li><a href="#plans">Plans</a></li>
           <li><a href="#contact">Contact</a></li>
-        </ul>
+        </ul> */}
       </div>
     </nav>
   );
